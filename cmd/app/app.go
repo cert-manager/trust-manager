c0c35e47844cd0d7bc629fbbaca2128fce062bea
joshvanl <vleeuwenjoshua@gmail.com>
2021-07-09 19:42:36 +0100

    Adds CLI and entry point for Bundle controller
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

---
+++ cmd/app/app.go
@@ -0,0 +1,113 @@
+/*
+Copyright 2021 The cert-manager Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package app
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	clientv1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/record"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/cert-manager/trust/cmd/app/options"
+	trustapi "github.com/cert-manager/trust/pkg/apis/trust/v1alpha1"
+	"github.com/cert-manager/trust/pkg/bundle"
+	"github.com/cert-manager/trust/pkg/webhook"
+)
+
+const (
+	helpOutput = "cert-manager istio agent for signing istio agent certificate signing requests through cert-manager"
+)
+
+// NewCommand will return a new command instance for the istio agent.
+func NewCommand() *cobra.Command {
+	opts := options.New()
+
+	cmd := &cobra.Command{
+		Use:   "cert-manager-trust",
+		Short: helpOutput,
+		Long:  helpOutput,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := opts.Complete(); err != nil {
+				return err
+			}
+
+			intscheme := runtime.NewScheme()
+			if err := scheme.AddToScheme(intscheme); err != nil {
+				return fmt.Errorf("failed to add k8s.io scheme: %s", err)
+			}
+			if err := trustapi.AddToScheme(intscheme); err != nil {
+				return fmt.Errorf("failed to add trust.cert-manager.io scheme: %s", err)
+			}
+
+			cl, err := kubernetes.NewForConfig(opts.RestConfig)
+			if err != nil {
+				return fmt.Errorf("error creating kubernetes client: %s", err.Error())
+			}
+
+			mlog := opts.Logr.WithName("trust")
+			eventBroadcaster := record.NewBroadcaster()
+			eventBroadcaster.StartLogging(func(format string, args ...interface{}) { mlog.V(3).Info(fmt.Sprintf(format, args...)) })
+			eventBroadcaster.StartRecordingToSink(&clientv1.EventSinkImpl{Interface: cl.CoreV1().Events(opts.Bundle.Namespace)})
+
+			mgr, err := ctrl.NewManager(opts.RestConfig, ctrl.Options{
+				Scheme: intscheme,
+				//
+				EventBroadcaster: eventBroadcaster,
+				//
+				LeaderElection:                true,
+				LeaderElectionNamespace:       opts.Bundle.Namespace,
+				LeaderElectionID:              "cert-manager-trust",
+				LeaderElectionReleaseOnCancel: true,
+				//
+				ReadinessEndpointName:  opts.ReadyzPath,
+				HealthProbeBindAddress: fmt.Sprintf("0.0.0.0:%d", opts.ReadyzPort),
+				//
+				Port:    opts.Webhook.Port,
+				Host:    opts.Webhook.Host,
+				CertDir: opts.Webhook.CertDir,
+				//
+				MetricsBindAddress: fmt.Sprintf("0.0.0.0:%d", opts.MetricsPort),
+				Logger:             mlog,
+			})
+			if err != nil {
+				return fmt.Errorf("failed to create manager: %w", err)
+			}
+
+			ctx := ctrl.SetupSignalHandler()
+
+			// Add Bundle controller to manager.
+			if err := bundle.AddBundleController(ctx, mgr, opts.Bundle); err != nil {
+				return fmt.Errorf("failed to register Bundle controller: %w", err)
+			}
+
+			// Register webhook handlers with manager.
+			webhook.Register(mgr, webhook.Options{Log: opts.Logr.WithName("webhook")})
+
+			// Start all runnables and controller
+			return mgr.Start(ctx)
+		},
+	}
+
+	opts = opts.Prepare(cmd)
+
+	return cmd
+}
---
+++ cmd/app/options/options.go
@@ -0,0 +1,159 @@
+/*
+Copyright 2021 The cert-manager Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package options
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/rest"
+	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/klog/v2"
+	"k8s.io/klog/v2/klogr"
+
+	"github.com/cert-manager/trust/pkg/bundle"
+)
+
+// Options is a struct to hold options for cert-manager-istio-csr
+type Options struct {
+	logLevel        string
+	kubeConfigFlags *genericclioptions.ConfigFlags
+
+	// ReadyzPort if the port used to expose Prometheus metrics.
+	ReadyzPort int
+	// ReadyzPath if the HTTP path used to expose Prometheus metrics.
+	ReadyzPath string
+
+	// MetricsPort is the port for exposing Prometheus metrics on 0.0.0.0 on the
+	// path '/metrics'.
+	MetricsPort int
+
+	// Logr is the shared base logger.
+	Logr logr.Logger
+
+	// RestConfig is the shared based rest config to connect to the Kubernetes
+	// API.
+	RestConfig *rest.Config
+
+	// Webhook are options specific to the Kubernetes Webhook.
+	Webhook
+
+	// Bundle are options specific to the Bundle controller.
+	Bundle bundle.Options
+}
+
+// TODO
+type Webhook struct {
+	Host    string
+	Port    int
+	CertDir string
+}
+
+func New() *Options {
+	return new(Options)
+}
+
+func (o *Options) Prepare(cmd *cobra.Command) *Options {
+	o.addFlags(cmd)
+	return o
+}
+
+func (o *Options) Complete() error {
+	klog.InitFlags(nil)
+	log := klogr.New()
+	flag.Set("v", o.logLevel)
+	o.Logr = log
+
+	var err error
+	o.RestConfig, err = o.kubeConfigFlags.ToRESTConfig()
+	if err != nil {
+		return fmt.Errorf("failed to build kubernetes rest config: %s", err)
+	}
+
+	o.Bundle.Log = o.Logr
+
+	return nil
+}
+
+func (o *Options) addFlags(cmd *cobra.Command) {
+	var nfs cliflag.NamedFlagSets
+
+	o.addAppFlags(nfs.FlagSet("App"))
+	o.addBundleFlags(nfs.FlagSet("Bundle"))
+	o.kubeConfigFlags = genericclioptions.NewConfigFlags(true)
+	o.kubeConfigFlags.AddFlags(nfs.FlagSet("Kubernetes"))
+
+	usageFmt := "Usage:\n  %s\n"
+	cmd.SetUsageFunc(func(cmd *cobra.Command) error {
+		fmt.Fprintf(cmd.OutOrStderr(), usageFmt, cmd.UseLine())
+		cliflag.PrintSections(cmd.OutOrStderr(), nfs, 0)
+		return nil
+	})
+
+	cmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+		fmt.Fprintf(cmd.OutOrStdout(), "%s\n\n"+usageFmt, cmd.Long, cmd.UseLine())
+		cliflag.PrintSections(cmd.OutOrStdout(), nfs, 0)
+	})
+
+	fs := cmd.Flags()
+	for _, f := range nfs.FlagSets {
+		fs.AddFlagSet(f)
+	}
+}
+
+func (o *Options) addAppFlags(fs *pflag.FlagSet) {
+	fs.StringVarP(&o.logLevel,
+		"log-level", "v", "1",
+		"Log level (1-5).")
+
+	fs.IntVar(&o.ReadyzPort,
+		"readiness-probe-port", 6060,
+		"Port to expose the readiness probe.")
+
+	fs.StringVar(&o.ReadyzPath,
+		"readiness-probe-path", "/readyz",
+		"HTTP path to expose the readiness probe server.")
+
+	fs.IntVar(&o.MetricsPort,
+		"metrics-port", 9402,
+		"Port to expose Prometheus metrics on 0.0.0.0 on path '/metrics'.")
+}
+
+func (o *Options) addBundleFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&o.Bundle.Namespace,
+		"trust-namespace", "cert-manager",
+		"Namespace to source trust bundles from.")
+}
+
+func (o *Options) addWebhookFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&o.Webhook.Host,
+		"webhook-host", "0.0.0.0",
+		"Host to serve webhook.")
+	fs.IntVar(&o.Webhook.Port,
+		"webhook-port", 6443,
+		"Port to serve webhook.")
+	fs.StringVar(&o.Webhook.CertDir,
+		"webhook-certificate-dir", "/tls",
+		"Directory where the Webhook certificate and private key are located. "+
+			"Certificate and private key must be named 'tls.crt' and 'tls.key' "+
+			"respectively.")
+}
