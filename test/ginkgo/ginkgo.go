220af68795c66fdb2cf704318758af89d2b33343
joshvanl <vleeuwenjoshua@gmail.com>
2021-07-15 17:13:53 +0100

    Move ginkgo boilerplate and reporter into shared /test
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

---
+++ test/ginkgo/ginkgo.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2021 The cert-manager Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ginkgo
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/onsi/ginkgo"
+	ginkgoconfig "github.com/onsi/ginkgo/config"
+	"github.com/onsi/ginkgo/reporters"
+	"github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+func init() {
+	// Turn on verbose by default to get spec names
+	ginkgoconfig.DefaultReporterConfig.Verbose = true
+	// Turn on EmitSpecProgress to get spec progress (especially on interrupt)
+	ginkgoconfig.GinkgoConfig.EmitSpecProgress = true
+	// Randomize specs as well as suites
+	ginkgoconfig.GinkgoConfig.RandomizeAllSpecs = true
+
+	wait.ForeverTestTimeout = time.Second * 60
+}
+
+func RunSuite(t *testing.T, suiteName string) {
+	gomega.RegisterFailHandler(ginkgo.Fail)
+
+	junitPath := "../../_artifacts"
+	if path := os.Getenv("ARTIFACTS"); path != "" {
+		junitPath = path
+	}
+
+	junitReporter := reporters.NewJUnitReporter(filepath.Join(
+		junitPath,
+		fmt.Sprintf("junit-go-%s.xml", suiteName),
+	))
+
+	ginkgo.RunSpecsWithDefaultAndCustomReporters(t, suiteName, []ginkgo.Reporter{junitReporter})
+}
