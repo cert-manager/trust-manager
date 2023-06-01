59a8ba3d84349788926b4ddbd540d6cf488a6cfe
irbekrm <irbekrm@gmail.com>
2023-06-01 15:59:20 +0100

    c/r changes: adapt the new webhook setup mechanism
    As the mechanism that we have been using so far that depends on injector decoder injection has been removed
    
    Signed-off-by: irbekrm <irbekrm@gmail.com>

--- pkg/webhook/validation.go
+++ pkg/webhook/validation.go
@@ -18,18 +18,16 @@ package webhook
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"net/http"
 	"strconv"
 	"sync"
 
 	"github.com/go-logr/logr"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
-	"github.com/cert-manager/trust-manager/pkg/apis/trust"
 	trustapi "github.com/cert-manager/trust-manager/pkg/apis/trust/v1alpha1"
 )
 
@@ -37,65 +35,36 @@ import (
 type validator struct {
 	log logr.Logger
 
-	decoder *admission.Decoder
-
 	lock sync.RWMutex
 }
 
-// Handle is a Kubernetes validation webhook server handler. Returns an
-// admission response containing whether the request is allowed or not.
-func (v *validator) Handle(ctx context.Context, req admission.Request) admission.Response {
-	log := v.log.WithValues("name", req.Name)
-	log.V(2).Info("received validation request")
+var _ admission.CustomValidator = &validator{}
 
-	if req.RequestKind == nil {
-		return admission.Errored(http.StatusBadRequest, errors.New("no resource kind sent in request"))
-	}
-
-	var (
-		el  field.ErrorList
-		err error
-	)
-
-	switch *req.RequestKind {
-	case metav1.GroupVersionKind{Group: trust.GroupName, Version: "v1alpha1", Kind: "Bundle"}:
-		var bundle trustapi.Bundle
-
-		v.lock.RLock()
-		err := v.decoder.Decode(req, &bundle)
-		v.lock.RUnlock()
-
-		if err != nil {
-			log.Error(err, "failed to decode Bundle")
-			return admission.Errored(http.StatusBadRequest, err)
-		}
-
-		el, err = v.validateBundle(ctx, &bundle)
-
-	default:
-		return admission.Denied(fmt.Sprintf("validation request for unrecognised resource type: %s/%s %s", req.RequestKind.Group, req.RequestKind.Version, req.RequestKind.Kind))
-	}
-
-	if err != nil {
-		log.Error(err, "internal error occurred validating request")
-		return admission.Errored(http.StatusInternalServerError, err)
-	}
-
-	if err := el.ToAggregate(); err != nil {
-		v.log.V(2).Info("denied request", "reason", el.ToAggregate().Error())
-		return admission.Denied(el.ToAggregate().Error())
-	}
-
-	log.V(2).Info("allowed request")
-	return admission.Allowed("Bundle validated")
+func (v *validator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	return v.validate(ctx, obj)
 }
 
-// validateBundle validates the incoming Bundle object and returns any
-// resulting error.
-func (v *validator) validateBundle(ctx context.Context, bundle *trustapi.Bundle) (field.ErrorList, error) {
-	var el field.ErrorList
-	path := field.NewPath("spec")
+func (v *validator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+	return v.validate(ctx, newObj)
+}
 
+func (v *validator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	// always allow deletes
+	return nil, nil
+}
+
+func (v *validator) validate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	bundle, ok := obj.(*trustapi.Bundle)
+	if !ok {
+		return nil, fmt.Errorf("expected a Bundle, but got a %T", obj)
+	}
+	log := v.log.WithValues("name", bundle.Name)
+	log.V(2).Info("received validation request")
+	var (
+		el       field.ErrorList
+		warnings admission.Warnings
+		path     = field.NewPath("spec")
+	)
 	if len(bundle.Spec.Sources) == 0 {
 		el = append(el, field.Forbidden(path.Child("sources"), "must define at least one source"))
 	} else {
@@ -191,28 +160,6 @@ func (v *validator) validateBundle(ctx context.Context, bundle *trustapi.Bundle)
 		conditionTypes[condition.Type] = struct{}{}
 	}
 
-	return el, nil
-}
+	return warnings, el.ToAggregate()
 
-// InjectDecoder is used by the controller-runtime manager to inject an object
-// decoder to convert into know trust.cert-manager.io types.
-func (v *validator) InjectDecoder(d *admission.Decoder) error {
-	v.lock.Lock()
-	defer v.lock.Unlock()
-
-	v.decoder = d
-	return nil
-}
-
-// check is used by the shared readiness manager to expose whether the server
-// is ready.
-func (v *validator) check(_ *http.Request) error {
-	v.lock.RLock()
-	defer v.lock.RUnlock()
-
-	if v.decoder != nil {
-		return nil
-	}
-
-	return errors.New("not ready")
 }
