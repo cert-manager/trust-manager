2c00131f14700a61e2f0bee4a37e8c5f98f783c4
Ashley Davis <ashley.davis@cyberark.com>
2025-02-04 11:41:00 +0000

    use the debian bookworm package for testing + releases
    Signed-off-by: Ashley Davis <ashley.davis@cyberark.com>

--- make/00_mod.mk
+++ make/00_mod.mk
@@ -67,7 +67,7 @@ define helm_values_mutation_function
 $(YQ) \
 	'( .image.repository = "$(oci_manager_image_name)" ) | \
 	( .image.tag = "$(oci_manager_image_tag)" ) | \
-	( .defaultPackageImage.repository = "$(oci_package_debian_image_name)" ) | \
-	( .defaultPackageImage.tag = "$(oci_package_debian_image_tag)" )' \
+	( .defaultPackageImage.repository = "$(oci_package_debian_bookworm_image_name)" ) | \
+	( .defaultPackageImage.tag = "$(oci_package_debian_bookworm_image_tag)" )' \
 	$1 --inplace
 endef
--- make/test-smoke.mk
+++ make/test-smoke.mk
@@ -46,7 +46,7 @@ endif
 
 test-smoke-deps: INSTALL_OPTIONS :=
 test-smoke-deps: INSTALL_OPTIONS += --set image.repository=$(oci_manager_image_name_development)
-test-smoke-deps: INSTALL_OPTIONS += --set defaultPackageImage.repository=$(oci_package_debian_image_name_development)
+test-smoke-deps: INSTALL_OPTIONS += --set defaultPackageImage.repository=$(oci_package_debian_bookworm_image_name_development)
 test-smoke-deps: INSTALL_OPTIONS += --set secretTargets.enabled=true --set secretTargets.authorizedSecretsAll=true
 test-smoke-deps: smoke-setup-cert-manager
 test-smoke-deps: install
