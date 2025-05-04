2a5efad8cbb1f85694280fb01e9a857e62541181
Erik Godding Boye <egboye@gmail.com>
2025-05-04 12:00:40 +0200

    Improve source error handling
    Signed-off-by: Erik Godding Boye <egboye@gmail.com>

--- pkg/bundle/source.go
+++ pkg/bundle/source.go
@@ -18,9 +18,7 @@ package bundle
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -35,7 +33,7 @@ import (
 
 type notFoundError struct{ error }
 
-type selectsNothingError struct{ error }
+type invalidSourcePEMError struct{ error }
 
 type invalidSecretSourceError struct{ error }
 
@@ -49,8 +47,7 @@ type bundleData struct {
 }
 
 // buildSourceBundle retrieves and concatenates all source bundle data for this Bundle object.
-// Each source data is validated and pruned to ensure that all certificates within are valid, and
-// is each bundle is concatenated together with a new line character.
+// Each source data is validated and pruned to ensure that all certificates within are valid.
 func (b *bundle) buildSourceBundle(ctx context.Context, sources []trustapi.BundleSource, formats *trustapi.AdditionalFormats) (bundleData, error) {
 	var resolvedBundle bundleData
 	certPool := util.NewCertPool(
@@ -59,52 +56,39 @@ func (b *bundle) buildSourceBundle(ctx context.Context, sources []trustapi.Bundl
 	)
 
 	for _, source := range sources {
-		var (
-			sourceData string
-			err        error
-		)
+		var certSource bundleSource
 
 		switch {
 		case source.ConfigMap != nil:
-			sourceData, err = b.configMapBundle(ctx, source.ConfigMap)
+			certSource = &configMapBundleSource{b, source.ConfigMap}
 
 		case source.Secret != nil:
-			sourceData, err = b.secretBundle(ctx, source.Secret)
+			certSource = &secretBundleSource{b, source.Secret}
 
 		case source.InLine != nil:
-			sourceData = *source.InLine
+			certSource = &inlineBundleSource{*source.InLine}
 
 		case source.UseDefaultCAs != nil:
 			if !*source.UseDefaultCAs {
 				continue
 			}
-
 			if b.defaultPackage == nil {
-				err = notFoundError{fmt.Errorf("no default package was specified when trust-manager was started; default CAs not available")}
-			} else {
-				sourceData = b.defaultPackage.Bundle
-				resolvedBundle.defaultCAPackageStringID = b.defaultPackage.StringID()
+				return bundleData{}, notFoundError{fmt.Errorf("no default package was specified when trust-manager was started; default CAs not available")}
 			}
+			certSource = &defaultCAsBundleSource{b.defaultPackage.Bundle}
+			resolvedBundle.defaultCAPackageStringID = b.defaultPackage.StringID()
+		default:
+			panic(fmt.Sprintf("don't know how to process source: %+v", source))
 		}
 
-		// A source selector may select no configmaps/secrets, and this is not an error.
-		if errors.As(err, &selectsNothingError{}) {
-			logf.FromContext(ctx).Info(err.Error())
-			continue
-		}
-
-		if err != nil {
-			return bundleData{}, fmt.Errorf("failed to retrieve bundle from source: %w", err)
-		}
-
-		if err := certPool.AddCertsFromPEM([]byte(sourceData)); err != nil {
-			return bundleData{}, fmt.Errorf("invalid PEM data in source: %w", err)
+		if err := certSource.addToCertPool(ctx, certPool); err != nil {
+			return bundleData{}, err
 		}
 	}
 
-	// NB: empty bundles are not valid so check and return an error if one somehow snuck through.
+	// NB: empty bundles are not valid, so check and return an error if one somehow snuck through.
 	if certPool.Size() == 0 {
-		return bundleData{}, fmt.Errorf("couldn't find any valid certificates in bundle")
+		return bundleData{}, notFoundError{fmt.Errorf("couldn't find any valid certificates in bundle")}
 	}
 
 	if err := resolvedBundle.Data.Populate(certPool, formats); err != nil {
@@ -114,115 +98,157 @@ func (b *bundle) buildSourceBundle(ctx context.Context, sources []trustapi.Bundl
 	return resolvedBundle, nil
 }
 
-// configMapBundle returns the data in the source ConfigMap within the trust Namespace.
-func (b *bundle) configMapBundle(ctx context.Context, ref *trustapi.SourceObjectKeySelector) (string, error) {
+type bundleSource interface {
+	addToCertPool(context.Context, *util.CertPool) error
+}
+
+type inlineBundleSource struct {
+	pemData string
+}
+
+func (s inlineBundleSource) addToCertPool(_ context.Context, pool *util.CertPool) error {
+	if err := pool.AddCertsFromPEM([]byte(s.pemData)); err != nil {
+		return invalidSourcePEMError{fmt.Errorf("inline source contains invalid PEM data: %w", err)}
+	}
+	return nil
+}
+
+type defaultCAsBundleSource struct {
+	pemData string
+}
+
+func (s defaultCAsBundleSource) addToCertPool(_ context.Context, pool *util.CertPool) error {
+	if err := pool.AddCertsFromPEM([]byte(s.pemData)); err != nil {
+		return invalidSourcePEMError{fmt.Errorf("default package contains invalid PEM data: %w", err)}
+	}
+	return nil
+}
+
+type configMapBundleSource struct {
+	*bundle
+	ref *trustapi.SourceObjectKeySelector
+}
+
+func (b configMapBundleSource) addToCertPool(ctx context.Context, pool *util.CertPool) error {
 	// this slice will contain a single ConfigMap if we fetch by name
 	// or potentially multiple ConfigMaps if we fetch by label selector
 	var configMaps []corev1.ConfigMap
 
 	// if Name is set, we `Get` by name
-	if ref.Name != "" {
+	if b.ref.Name != "" {
 		cm := corev1.ConfigMap{}
 		if err := b.client.Get(ctx, client.ObjectKey{
 			Namespace: b.Namespace,
-			Name:      ref.Name,
-		}, &cm); apierrors.IsNotFound(err) {
-			return "", notFoundError{err}
-		} else if err != nil {
-			return "", fmt.Errorf("failed to get ConfigMap %s/%s: %w", b.Namespace, ref.Name, err)
+			Name:      b.ref.Name,
+		}, &cm); err != nil {
+			err = fmt.Errorf("failed to get ConfigMap %s/%s: %w", b.Namespace, b.ref.Name, err)
+			if apierrors.IsNotFound(err) {
+				err = notFoundError{err}
+			}
+			return err
 		}
 
 		configMaps = []corev1.ConfigMap{cm}
 	} else {
 		// if Selector is set, we `List` by label selector
 		cml := corev1.ConfigMapList{}
-		selector, selectorErr := metav1.LabelSelectorAsSelector(ref.Selector)
+		selector, selectorErr := metav1.LabelSelectorAsSelector(b.ref.Selector)
 		if selectorErr != nil {
-			return "", fmt.Errorf("failed to parse label selector as Selector for ConfigMap in namespace %s: %w", b.Namespace, selectorErr)
+			return fmt.Errorf("failed to parse label selector as Selector for ConfigMap in namespace %s: %w", b.Namespace, selectorErr)
 		}
 		if err := b.client.List(ctx, &cml, client.MatchingLabelsSelector{Selector: selector}); err != nil {
-			return "", fmt.Errorf("failed to get ConfigMapList: %w", err)
+			return fmt.Errorf("failed to get ConfigMapList: %w", err)
 		} else if len(cml.Items) == 0 {
-			return "", selectsNothingError{fmt.Errorf("label selector %s for ConfigMap didn't match any resources", selector.String())}
+			logf.FromContext(ctx).Info(fmt.Sprintf("label selector %s for ConfigMap didn't match any resources", selector.String()))
+			return nil
 		}
 
 		configMaps = cml.Items
 	}
 
-	var results strings.Builder
 	for _, cm := range configMaps {
-		if len(ref.Key) > 0 {
-			data, ok := cm.Data[ref.Key]
+		if len(b.ref.Key) > 0 {
+			data, ok := cm.Data[b.ref.Key]
 			if !ok {
-				return "", notFoundError{fmt.Errorf("no data found in ConfigMap %s/%s at key %q", cm.Namespace, cm.Name, ref.Key)}
+				return notFoundError{fmt.Errorf("no data found in ConfigMap %s/%s at key %q", cm.Namespace, cm.Name, b.ref.Key)}
 			}
-			results.WriteString(data)
-			results.WriteByte('\n')
-		} else if ref.IncludeAllKeys {
-			for _, data := range cm.Data {
-				results.WriteString(data)
-				results.WriteByte('\n')
+			if err := pool.AddCertsFromPEM([]byte(data)); err != nil {
+				return invalidSourcePEMError{fmt.Errorf("invalid PEM data in ConfigMap %s/%s at key %q: %w", cm.Namespace, cm.Name, b.ref.Key, err)}
+			}
+		} else if b.ref.IncludeAllKeys {
+			for key, data := range cm.Data {
+				if err := pool.AddCertsFromPEM([]byte(data)); err != nil {
+					return invalidSourcePEMError{fmt.Errorf("invalid PEM data in ConfigMap %s/%s at key %q: %w", cm.Namespace, cm.Name, key, err)}
+				}
 			}
 		}
 	}
-	return results.String(), nil
+	return nil
 }
 
-// secretBundle returns the data in the source Secret within the trust Namespace.
-func (b *bundle) secretBundle(ctx context.Context, ref *trustapi.SourceObjectKeySelector) (string, error) {
+type secretBundleSource struct {
+	*bundle
+	ref *trustapi.SourceObjectKeySelector
+}
+
+func (b secretBundleSource) addToCertPool(ctx context.Context, pool *util.CertPool) error {
 	// this slice will contain a single Secret if we fetch by name
 	// or potentially multiple Secrets if we fetch by label selector
 	var secrets []corev1.Secret
 
 	// if Name is set, we `Get` by name
-	if ref.Name != "" {
+	if b.ref.Name != "" {
 		s := corev1.Secret{}
 		if err := b.client.Get(ctx, client.ObjectKey{
 			Namespace: b.Namespace,
-			Name:      ref.Name,
-		}, &s); apierrors.IsNotFound(err) {
-			return "", notFoundError{err}
-		} else if err != nil {
-			return "", fmt.Errorf("failed to get Secret %s/%s: %w", b.Namespace, ref.Name, err)
+			Name:      b.ref.Name,
+		}, &s); err != nil {
+			err = fmt.Errorf("failed to get Secret %s/%s: %w", b.Namespace, b.ref.Name, err)
+			if apierrors.IsNotFound(err) {
+				err = notFoundError{err}
+			}
+			return err
 		}
 
 		secrets = []corev1.Secret{s}
 	} else {
 		// if Selector is set, we `List` by label selector
 		sl := corev1.SecretList{}
-		selector, selectorErr := metav1.LabelSelectorAsSelector(ref.Selector)
+		selector, selectorErr := metav1.LabelSelectorAsSelector(b.ref.Selector)
 		if selectorErr != nil {
-			return "", fmt.Errorf("failed to parse label selector as Selector for Secret in namespace %s: %w", b.Namespace, selectorErr)
+			return fmt.Errorf("failed to parse label selector as Selector for Secret in namespace %s: %w", b.Namespace, selectorErr)
 		}
 		if err := b.client.List(ctx, &sl, client.MatchingLabelsSelector{Selector: selector}); err != nil {
-			return "", fmt.Errorf("failed to get SecretList: %w", err)
+			return fmt.Errorf("failed to get SecretList: %w", err)
 		} else if len(sl.Items) == 0 {
-			return "", selectsNothingError{fmt.Errorf("label selector %s for Secret didn't match any resources", selector.String())}
+			logf.FromContext(ctx).Info(fmt.Sprintf("label selector %s for Secret didn't match any resources", selector.String()))
+			return nil
 		}
 
 		secrets = sl.Items
 	}
 
-	var results strings.Builder
 	for _, secret := range secrets {
-		if len(ref.Key) > 0 {
-			data, ok := secret.Data[ref.Key]
+		if len(b.ref.Key) > 0 {
+			data, ok := secret.Data[b.ref.Key]
 			if !ok {
-				return "", notFoundError{fmt.Errorf("no data found in Secret %s/%s at key %q", secret.Namespace, secret.Name, ref.Key)}
+				return notFoundError{fmt.Errorf("no data found in Secret %s/%s at key %q", secret.Namespace, secret.Name, b.ref.Key)}
 			}
-			results.Write(data)
-			results.WriteByte('\n')
-		} else if ref.IncludeAllKeys {
+			if err := pool.AddCertsFromPEM(data); err != nil {
+				return invalidSourcePEMError{fmt.Errorf("invalid PEM data in Secret %s/%s at key %q: %w", secret.Namespace, secret.Name, b.ref.Key, err)}
+			}
+		} else if b.ref.IncludeAllKeys {
 			// This is done to prevent mistakes. All keys should never be included for a TLS secret, since that would include the private key.
 			if secret.Type == corev1.SecretTypeTLS {
-				return "", invalidSecretSourceError{fmt.Errorf("includeAllKeys is not supported for TLS Secrets such as %s/%s", secret.Namespace, secret.Name)}
+				return invalidSecretSourceError{fmt.Errorf("includeAllKeys is not supported for TLS Secrets such as %s/%s", secret.Namespace, secret.Name)}
 			}
 
-			for _, data := range secret.Data {
-				results.Write(data)
-				results.WriteByte('\n')
+			for key, data := range secret.Data {
+				if err := pool.AddCertsFromPEM(data); err != nil {
+					return invalidSourcePEMError{fmt.Errorf("invalid PEM data in Secret %s/%s at key %q: %w", secret.Namespace, secret.Name, key, err)}
+				}
 			}
 		}
 	}
-	return results.String(), nil
+	return nil
 }
--- pkg/bundle/source_test.go
+++ pkg/bundle/source_test.go
@@ -56,34 +56,24 @@ func Test_buildSourceBundle(t *testing.T) {
 		expPKCS12   bool
 		expPassword *string
 	}{
-		"if no sources defined, should return an error": {
-			objects:          []runtime.Object{},
-			expData:          "",
+		"if no sources defined, should return notFoundError": {
 			expError:         true,
-			expNotFoundError: false,
+			expNotFoundError: true,
 		},
 		"if single InLine source defined with newlines, should trim and return": {
 			sources: []trustapi.BundleSource{
-				{InLine: ptr.To(dummy.TestCertificate1 + "\n" + dummy.TestCertificate2 + "\n\n")},
+				{InLine: ptr.To(dummy.TestCertificate1 + "\n" + dummy.TestCertificate2 + "\n")},
 			},
-			objects:          []runtime.Object{},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
 		},
 		"if single DefaultPackage source defined, should return": {
-			sources:          []trustapi.BundleSource{{UseDefaultCAs: ptr.To(true)}},
-			objects:          []runtime.Object{},
-			expData:          dummy.JoinCerts(dummy.TestCertificate5),
-			expError:         false,
-			expNotFoundError: false,
+			sources: []trustapi.BundleSource{{UseDefaultCAs: ptr.To(true)}},
+			expData: dummy.JoinCerts(dummy.TestCertificate5),
 		},
 		"if single ConfigMap source which doesn't exist, return notFoundError": {
 			sources: []trustapi.BundleSource{
 				{ConfigMap: &trustapi.SourceObjectKeySelector{Name: "configmap", Key: "key"}},
 			},
-			objects:          []runtime.Object{},
-			expData:          "",
 			expError:         true,
 			expNotFoundError: true,
 		},
@@ -92,7 +82,6 @@ func Test_buildSourceBundle(t *testing.T) {
 				{ConfigMap: &trustapi.SourceObjectKeySelector{Name: "configmap", Key: "key"}},
 			},
 			objects:          []runtime.Object{&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Name: "configmap"}}},
-			expData:          "",
 			expError:         true,
 			expNotFoundError: true,
 		},
@@ -104,9 +93,7 @@ func Test_buildSourceBundle(t *testing.T) {
 				ObjectMeta: metav1.ObjectMeta{Name: "configmap"},
 				Data:       map[string]string{"key": dummy.TestCertificate1 + "\n" + dummy.TestCertificate2},
 			}},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
 		},
 		"if single ConfigMap source including all keys, return data": {
 			sources: []trustapi.BundleSource{
@@ -116,9 +103,7 @@ func Test_buildSourceBundle(t *testing.T) {
 				ObjectMeta: metav1.ObjectMeta{Name: "configmap"},
 				Data:       map[string]string{"cert-1": dummy.TestCertificate1 + "\n" + dummy.TestCertificate2, "cert-2": dummy.TestCertificate3},
 			}},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1, dummy.TestCertificate3),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1, dummy.TestCertificate3),
 		},
 		"if single ConfigMap source, return data even when order changes": {
 			// Test uses the same data as the previous one but with different order
@@ -129,18 +114,14 @@ func Test_buildSourceBundle(t *testing.T) {
 				ObjectMeta: metav1.ObjectMeta{Name: "configmap"},
 				Data:       map[string]string{"key": dummy.TestCertificate2 + "\n" + dummy.TestCertificate1},
 			}},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
 		},
-		"if selects no ConfigMap sources, should return an error": {
+		"if selects no ConfigMap sources, should return notFoundError": {
 			sources: []trustapi.BundleSource{
 				{ConfigMap: &trustapi.SourceObjectKeySelector{Key: "key", Selector: &metav1.LabelSelector{MatchLabels: map[string]string{"selects-nothing": "true"}}}},
 			},
-			objects:          []runtime.Object{},
-			expData:          "",
 			expError:         true,
-			expNotFoundError: false,
+			expNotFoundError: true,
 		},
 		"if selects at least one ConfigMap source, return data": {
 			sources: []trustapi.BundleSource{
@@ -151,9 +132,7 @@ func Test_buildSourceBundle(t *testing.T) {
 				ObjectMeta: metav1.ObjectMeta{Name: "configmap", Labels: map[string]string{"trust-bundle.certs": "includes"}},
 				Data:       map[string]string{"key": dummy.TestCertificate1 + "\n" + dummy.TestCertificate2},
 			}},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
 		},
 		"if selects at least one ConfigMap source including all keys, return data": {
 			sources: []trustapi.BundleSource{
@@ -173,9 +152,7 @@ func Test_buildSourceBundle(t *testing.T) {
 						"cert-4": dummy.TestCertificate4,
 					},
 				}},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1, dummy.TestCertificate4, dummy.TestCertificate3),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1, dummy.TestCertificate4, dummy.TestCertificate3),
 		},
 		"if ConfigMap and InLine source, return concatenated data": {
 			sources: []trustapi.BundleSource{
@@ -186,16 +163,12 @@ func Test_buildSourceBundle(t *testing.T) {
 				ObjectMeta: metav1.ObjectMeta{Name: "configmap"},
 				Data:       map[string]string{"key": dummy.TestCertificate1},
 			}},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
 		},
 		"if single Secret source exists which doesn't exist, should return not found error": {
 			sources: []trustapi.BundleSource{
 				{Secret: &trustapi.SourceObjectKeySelector{Name: "secret", Key: "key"}},
 			},
-			objects:          []runtime.Object{},
-			expData:          "",
 			expError:         true,
 			expNotFoundError: true,
 		},
@@ -204,7 +177,6 @@ func Test_buildSourceBundle(t *testing.T) {
 				{Secret: &trustapi.SourceObjectKeySelector{Name: "secret", Key: "key"}},
 			},
 			objects:          []runtime.Object{&corev1.Secret{ObjectMeta: metav1.ObjectMeta{Name: "secret"}}},
-			expData:          "",
 			expError:         true,
 			expNotFoundError: true,
 		},
@@ -217,7 +189,6 @@ func Test_buildSourceBundle(t *testing.T) {
 				Type:       corev1.SecretTypeTLS,
 				Data:       map[string][]byte{"cert-1": []byte(dummy.TestCertificate1), "cert-2": []byte(dummy.TestCertificate2)},
 			}},
-			expData:                     "",
 			expError:                    true,
 			expInvalidSecretSourceError: true,
 		},
@@ -229,9 +200,7 @@ func Test_buildSourceBundle(t *testing.T) {
 				ObjectMeta: metav1.ObjectMeta{Name: "secret"},
 				Data:       map[string][]byte{"key": []byte(dummy.TestCertificate1 + "\n" + dummy.TestCertificate2)},
 			}},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
 		},
 		"if single Secret source including all keys, return data": {
 			sources: []trustapi.BundleSource{
@@ -241,9 +210,7 @@ func Test_buildSourceBundle(t *testing.T) {
 				ObjectMeta: metav1.ObjectMeta{Name: "secret"},
 				Data:       map[string][]byte{"cert-1": []byte(dummy.TestCertificate1 + "\n" + dummy.TestCertificate2), "cert-9": []byte(dummy.TestCertificate4)},
 			}},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1, dummy.TestCertificate4),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1, dummy.TestCertificate4),
 		},
 		"if Secret and InLine source, return concatenated data": {
 			sources: []trustapi.BundleSource{
@@ -254,9 +221,7 @@ func Test_buildSourceBundle(t *testing.T) {
 				ObjectMeta: metav1.ObjectMeta{Name: "secret"},
 				Data:       map[string][]byte{"key": []byte(dummy.TestCertificate2)},
 			}},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1),
 		},
 		"if Secret, ConfigMap and InLine source, return concatenated data": {
 			sources: []trustapi.BundleSource{
@@ -274,9 +239,7 @@ func Test_buildSourceBundle(t *testing.T) {
 					Data:       map[string][]byte{"key": []byte(dummy.TestCertificate2)},
 				},
 			},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1, dummy.TestCertificate3),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1, dummy.TestCertificate3),
 		},
 		"if source Secret exists, but not ConfigMap, return not found error": {
 			sources: []trustapi.BundleSource{
@@ -289,7 +252,6 @@ func Test_buildSourceBundle(t *testing.T) {
 					Data:       map[string]string{"key": dummy.TestCertificate1},
 				},
 			},
-			expData:          "",
 			expError:         true,
 			expNotFoundError: true,
 		},
@@ -304,7 +266,6 @@ func Test_buildSourceBundle(t *testing.T) {
 					Data:       map[string][]byte{"key": []byte(dummy.TestCertificate1)},
 				},
 			},
-			expData:          "",
 			expError:         true,
 			expNotFoundError: true,
 		},
@@ -326,9 +287,7 @@ func Test_buildSourceBundle(t *testing.T) {
 						"cert-4": []byte(dummy.TestCertificate4),
 					},
 				}},
-			expData:          dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1, dummy.TestCertificate4, dummy.TestCertificate3),
-			expError:         false,
-			expNotFoundError: false,
+			expData: dummy.JoinCerts(dummy.TestCertificate2, dummy.TestCertificate1, dummy.TestCertificate4, dummy.TestCertificate3),
 		},
 		"if selects at least one Secret source of type TLS including all keys, return invalidSecretSourceError": {
 			sources: []trustapi.BundleSource{
@@ -349,7 +308,6 @@ func Test_buildSourceBundle(t *testing.T) {
 						"cert-4": []byte(dummy.TestCertificate4),
 					},
 				}},
-			expData:                     "",
 			expError:                    true,
 			expInvalidSecretSourceError: true,
 		},
