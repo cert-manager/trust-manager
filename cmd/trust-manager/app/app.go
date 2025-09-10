0860ab56761b9c0d07cbbdbcb5beb383f843458a
Abdellah Smaoune <asmaoune@vollivier-1715778767.vm.vdi.s1.p.fti.net>
2025-09-10 16:25:53 +0200

    use the targetNamespaces new parameter when calling some functions
    Signed-off-by: Abdellah Smaoune <abdellah.smaoune.ext@orange.com>

--- cmd/trust-manager/app/app.go
+++ cmd/trust-manager/app/app.go
@@ -89,7 +89,7 @@ func NewCommand() *cobra.Command {
 				Metrics: server.Options{
 					BindAddress: fmt.Sprintf("0.0.0.0:%d", opts.MetricsPort),
 				},
-				Cache: bundle.CacheOpts(opts.Bundle),
+				Cache: bundle.CacheOpts(opts.Bundle, opts.TargetNamespaces),
 			})
 			if err != nil {
 				return fmt.Errorf("failed to create manager: %w", err)
@@ -106,7 +106,7 @@ func NewCommand() *cobra.Command {
 			logf.IntoContext(ctx, log)
 
 			// Add Bundle controller to manager.
-			if err := bundle.SetupWithManager(ctx, mgr, opts.Bundle); err != nil {
+			if err := bundle.SetupWithManager(ctx, mgr, opts.Bundle, opts.TargetNamespaces); err != nil {
 				return fmt.Errorf("failed to register Bundle controller: %w", err)
 			}
 
